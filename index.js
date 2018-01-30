54c9e17f4a6aab651e9aba15dc33e54ccdc5ca80
Mattias Asplund <mattias@mattiasasplund.se>
2018-01-30 13:35:37 +0100

    check() reduce size by 25%

--- index.js
+++ index.js
@@ -10,32 +10,23 @@ exec('npm outdated -json > outdated.json', (err, stdout, stderr) => {
     }
     var outdatedJson = jsonfile.readFileSync('outdated.json')
     var packageJson = jsonfile.readFileSync('package.json')
-    if (packageJson.devDependencies !== null) {
-        console.log("devDependencies")
-        console.log("---------------")
-        for (var prop in packageJson.devDependencies) {
-            if(outdatedJson[prop] !== undefined &&
-                outdatedJson[prop].latest !== "git") {
-                    var msg = "Updating " + prop + " from " + packageJson.devDependencies[prop] +
-                    " to " + outdatedJson[prop].latest
-                packageJson.devDependencies[prop] = outdatedJson[prop].latest
-                console.log(msg)
-            }
-        }
-    }
-    if (outdatedJson.dependencies !== null) {
-        console.log("dependencies")
-        console.log("------------")
-        for (var prop in packageJson.dependencies) {
-                if(outdatedJson[prop] !== undefined && 
-                    outdatedJson[prop].latest !== "git") {
-                var msg = "Updating " + prop + " from " + packageJson.dependencies[prop] +
-                    " to " + outdatedJson[prop].latest
-                packageJson.dependencies[prop] = outdatedJson[prop].latest
-                console.log(msg)
-            }
-        }
-    }
+    check("devDependencies")
+    check("dependencies")
     jsonfile.writeFileSync('test.json', packageJson, { spaces: 2 })
 });
 
+function check(hive) {
+    if (packageJson[hive] !== null) {
+        console.log(hive)
+        console.log("-".repeat(hive.length))
+        for (var prop in packageJson[hive]) {
+            if(outdatedJson[prop] !== undefined &&
+                outdatedJson[prop].latest !== "git") {
+                    var msg = "Updating " + prop + " from " + packageJson[hive][prop] +
+                    " to " + outdatedJson[prop].latest
+                packageJson[hive][prop] = outdatedJson[prop].latest
+                console.log(msg)
+            }
+        }
+    }
+}
